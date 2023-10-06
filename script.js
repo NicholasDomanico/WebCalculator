b8aa7cfc7f02bbfc70b47a1601aae79f7f52a6c9
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2023-10-06 00:41:48 -0400

    Added math functionality Add, Subtract, Multiply, and Divide now function.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const subtract = document.getElementById("Subtract");
 const multiply = document.getElementById("Multiply");
 const divide = document.getElementById("Divide");
 
+let text = ""
 let currentSum = 0;
 let operand = 0;
 let operator = "";
@@ -14,22 +15,48 @@ let operator = "";
 let numbersArray = [...numberKeys];
 for(let i = 0; i < numbersArray.length; i++) {
     numbersArray[i].addEventListener("click", function() {
-        currentSum = currentSum + numbersArray[i].id;
-        display(Number(currentSum));
+        text = text + numbersArray[i].id;
+        operand = Number(text);
+        display(text);
     })
 }
 
 add.addEventListener("click", function() {
     operator = "+";
-    currentSum = Number(currentSum) += Number(operand);
+    currentSum = doMath(currentSum, operand);
+    operand = 0;
+    text = String(currentSum);
+    display(text);
+    console.log(operand, operator, currentSum);
+    text = "";
 })
 
 subtract.addEventListener("click", function() {
-    operator = "-";1
+    operator = "-";
     currentSum = currentSum -= Number(operand);
 })
 
+function doMath(operandA, operandB) {
+    if (operator === "") {
+        return null;
+    }
 
+    if (operator === "+") {
+        return operandA + operandB;
+    }
+
+    if (operator === "-") {
+        return operandA - operandB;
+    }
+
+    if (operator === "x") {
+        return operandA * operandB;
+    }
+
+    if (operator === "/") {
+        return operandA / operandB;
+    }
+}
 
 function display(number) {
     displayText.innerHTML = `${number}`;
